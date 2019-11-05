3dbddc71a1201bd2e4eca51f8ef5f5f8381c3b3a
dnalpert <36242436+dnalpert@users.noreply.github.com>
2019-11-05 08:38:50 -0800

    added getters to store

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <h1>Adopt a New Friend</h1>
+
+    {{ animalsCount }}
+
     <button
       class="btn btn-primary"
       @click="togglePetForm"
@@ -44,7 +47,7 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'home',
@@ -57,6 +60,11 @@ export default {
         species: null
       }
     }
+  },
+  computed: {
+    ...mapGetters([
+      'animalsCount'
+    ])
   },
   methods: {
     ...mapActions([
